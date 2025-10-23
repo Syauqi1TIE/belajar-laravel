d9fd0c92cbad0784594b424889382c158df883a1
Syauqi <syauqi24ti@mahasiswa.pcr.ac.id>
2025-10-23 10:51:25 +0700

    pertemuan7

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,50 @@
 <?php
 
-use Faker\Guesser\Name;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
-use App\Http\Controllers\MatakuliahController;
-use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\DashboardController;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+use App\Http\Controllers\QuestionController;
+
+Route::get('/pcr', function () {
+    return 'Selamat Datang di Website Kampus PCR!';
+});
+use App\Http\Controllers\MatakuliahController;
 
 Route::get('/mahasiswa', function () {
-    return 'Hallo mahasiswa';
+    return 'Hai Mahasiswa';
 });
 
-Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
+Route::get('/nama/{param1}', function ($param1) {
+    return 'Nama Saya: ' .$param1;
+});
+
+Route::get('/nim/{param1?}', function ($param1 = '') {
+    return 'Nim Saya: ' .$param1;
+});
+
+Route::get('/mahasiswa/{param1}', [MahasiswaController ::class,'show']) ;
 
 Route::get('/about', function () {
     return view('halaman-about');
 });
 
-Route::get('/matakuliah/{param1}', [MatakuliahController::class, 'show']);
-
+Route::get('/matakuliah/show/{param1?}', [MatakuliahController ::class,'show']) ;
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+        ->name('home');
 Route::post('question/store', [QuestionController::class, 'store'])
-		->name('question.store');
+        ->name('question.store');
 
-Route::get('/home', [HomeController::class, 'index'])
-->name('home');
-Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
+use App\Http\Controllers\AuthController;
+
+
+use App\Http\Controllers\PegawaiController;
+Route::get('/pegawai', [PegawaiController::class, 'form']);
+Route::post('/pegawai', [PegawaiController::class, 'index']);
+
+use App\Http\Controllers\DashboardController;
+Route::get('dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
+
+use App\Http\Controllers\PelangganController;
 Route::resource('pelanggan', PelangganController::class);
-
+// edcvefv
